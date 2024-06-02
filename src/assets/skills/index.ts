c345d35706dcf4f84a104cdb39ee55a9970b3c5f
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2024-06-02 15:41:58 +0530

    skill icons added

---
+++ src/assets/skills/index.ts
@@ -0,0 +1,39 @@
+import css from "./css.svg";
+import cpp from "./cpp.svg";
+import docker from "./docker.svg";
+import git from "./git.svg";
+import expressjs from "./expressjs.svg";
+import html from "./html.svg";
+import js from "./js.svg";
+import laravel from "./laravel.svg";
+import mongodb from "./mongodb.svg";
+import nextjs from "./nextjs.svg";
+import nodejs from "./nodejs.svg";
+import postman from "./postman.svg";
+import python from "./python.svg";
+import react from "./react.svg";
+import redux from "./redux.svg";
+import tailwindcss from "./tailwindcss.svg";
+import ts from "./ts.svg";
+import typescript from "./Typescript.svg";
+
+export {
+  css,
+  cpp,
+  docker,
+  git,
+  expressjs,
+  html,
+  js,
+  laravel,
+  mongodb,
+  nextjs,
+  nodejs,
+  postman,
+  python,
+  react,
+  redux,
+  tailwindcss,
+  ts,
+  typescript,
+};
